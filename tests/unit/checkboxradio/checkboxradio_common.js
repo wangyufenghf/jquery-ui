378a2096ab274718611f5aa2b68ab102a7727df0
Alexander Schmitz <arschmitz@gmail.com>
2014-08-28 15:16:51 -0400

    Checkboxradio: inital commit of new widget

---
+++ tests/unit/checkboxradio/checkboxradio_common.js
@@ -0,0 +1,11 @@
+TestHelpers.commonWidgetTests( "checkboxradio", {
+	noDefaultElement: true,
+	defaults: {
+		disabled: null,
+		label: null,
+		icon: false,
+
+		// Callbacks
+		create: null
+	}
+});
